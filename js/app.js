41d3d6dc1a4b76c422d5ba3ac4df06faf578dddd
karto <kartofelek007@gmail.com>
2022-01-02 12:15:44 +0100

    poprawki wygladu

--- js/app.js
+++ js/app.js
@@ -14,9 +14,10 @@ function copyAlert() {
 }
 
 function bindEventToIcons() {
-    const elements = document.querySelectorAll(".icon-el");
+    const elements = document.querySelectorAll(".icon");
     elements.forEach(el => {
-        el.onclick = () => {
+        el.querySelector(".icon-content").addEventListener("click", () => {
+            el.style.zIndex = 1000000;
             const anim = el.animate([
                 { transform: `scale(1)`, boxShadow: `inset 0 0 0 2px black` },
                 { transform: `scale(1.2)`, boxShadow: `inset 0 0 0 4px black` },
@@ -26,10 +27,68 @@ function bindEventToIcons() {
                 iterations: 1
             })
             anim.onfinish = e => {
+                el.style.zIndex = "";
                 copyTextToClipboard(el.querySelector(".icon-img").innerHTML.trim());
                 copyAlert();
             }
-        }
+        });
+
+        el.querySelector(".icon-checkbox").addEventListener("click", e => {
+            e.stopPropagation();
+
+            const item = localStorage.getItem("favorites");
+            let favorites = [];
+            if (item) {
+                favorites = JSON.parse(item);
+            }
+            const name = el.dataset.name;
+            const set = new Set(favorites);
+
+            if (e.target.checked) {
+                set.add(name);
+            } else {
+                set.delete(name);
+            }
+
+            localStorage.setItem("favorites", JSON.stringify([...set]));
+
+            enableDisableFavoriteBtn();
+            //automatycznie usuwa ikonke jak jest widok favorite
+            // if (!e.target.checked) {
+            //     showFavorites();
+            // }
+        })
+
+        el.querySelector(".icon-css").addEventListener("click", e => {
+            e.preventDefault();
+            e.stopPropagation();
+            let data = e.target.closest(".icon").querySelector(".icon-img svg").outerHTML.trim();
+            data = data.replaceAll(`"`, `'`);
+            data = data.replaceAll(">", "%3E");
+            data = data.replaceAll("<", "%3C");
+            data = data.replaceAll("#", "%23");
+            data = data.replace(/(?:\r\n|\r|\n)/g, '');
+            let text = `background-image: url("data:image/svg+xml,${data}");`;
+            copyTextToClipboard(text);
+            copyAlert();
+        })
+
+        el.querySelector(".icon-download").addEventListener("click", e => {
+            e.stopPropagation();
+            e.preventDefault();
+            const a = document.createElement('a');
+            const svgCode = el.querySelector(".icon-img svg").outerHTML.trim();
+
+            a.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(svgCode));
+            a.setAttribute('download', el.dataset.name);
+
+            a.style.display = 'none';
+            document.body.appendChild(a);
+
+            a.click();
+
+            document.body.removeChild(a);
+        })
     });
 }
 
@@ -49,9 +108,9 @@ function bindButtons() {
 
     function filterIcons() {
         const val = input.value;
-        const elements = document.querySelectorAll(".icon-el");
+        const elements = document.querySelectorAll(".icon");
         [...elements].forEach(div => {
-            const name = div.querySelector(".icon-name").innerHTML;
+            const name = div.dataset.name;
             div.hidden = !name.includes(val);
         });
     }
@@ -76,21 +135,108 @@ function bindButtons() {
         setLayout();
     }
 
+    document.querySelector("#favorite").onclick = e => {
+        e.currentTarget.classList.toggle("is-active");
+        if (e.currentTarget.classList.contains("is-active")) {
+            showFavorites();
+        } else {
+            const ev = new Event("input");
+            input.dispatchEvent(ev);
+        }
+    }
+
     setLayout();
 }
 
-const header = document.querySelector(".header");
+function showFavorites() {
+    const elements = document.querySelectorAll(".icon");
+    const item = localStorage.getItem("favorites");
+    if (item) {
+        const arr = JSON.parse(item);
+        if (arr.length) {
+            [...elements].forEach(div => {
+                const name = div.dataset.name;
+                div.hidden = !arr.includes(name);
+            });
+        }
+    }
+}
+
+function setFavoriteCheckbox() {
+    const elements = document.querySelectorAll(".icon");
+    const item = localStorage.getItem("favorites");
+    if (item) {
+        const arr = JSON.parse(item);
+        if (arr.length) {
+            [...elements].forEach(div => {
+                const name = div.dataset.name;
+                div.querySelector(".icon-checkbox").checked = arr.includes(name);
+            });
+        }
+    }
+}
+
 function stickyHeader() {
+    const header = document.querySelector(".header");
     window.onscroll = e => {
         header.classList.toggle("is-sticky", window.scrollY > 0);
     }
     header.classList.toggle("is-sticky", window.scrollY > 0);
 }
 
-function initApp() {
-    bindButtons();
-    bindEventToIcons();
+function enableDisableFavoriteBtn() {
+    const btn = document.querySelector("#favorite");
+    btn.disabled = true;
+    const item = localStorage.getItem("favorites");
+    if (item) {
+        const arr = JSON.parse(item);
+        if (arr.length) {
+            btn.disabled = false;
+        }
+    }
 }
 
-stickyHeader();
+function initColorPicker() {
+    var parent = document.querySelector('#colorPickerInside div');
+    var picker = new Picker({
+        parent: parent,
+        popup: false,
+        //color: 'black',
+        alpha: false,
+        editor: true,
+        editorFormat: 'hex',
+        //cancelButton : true,
+        onDone: function(color) {
+            const el = document.querySelectorAll(".icon");
+            el.forEach(el => {
+                const svg = el.querySelector(".icon-img svg");
+                const svgOriginal = el.querySelector(".icon-img-original svg");
+                svg.outerHTML = svgOriginal.outerHTML.replaceAll('currentColor', color.hex.slice(0, 7));
+            })
+        },
+    });
+
+    document.querySelector("#colorPicker").addEventListener("click", e => {
+        e.stopPropagation();
+        e.currentTarget.classList.toggle("is-active");
+    })
+
+    document.querySelector("#colorPickerInside").addEventListener("click", e => {
+        e.stopPropagation();
+    })
+
+    document.addEventListener("click", e => {
+        document.querySelector("#colorPicker").classList.remove("is-active");
+    })
+}
+
+function initApp() {
+    initColorPicker();
+    stickyHeader();
+    setFavoriteCheckbox();
+    bindButtons();
+    bindEventToIcons();
+    enableDisableFavoriteBtn();
+}
+
 initApp();
